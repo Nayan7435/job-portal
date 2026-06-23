b126429a28a84a10ffb487250e7799bc0c76969e
Gohel Nayan <gohelnayan264-gmail@gmail.com>
2026-06-23 14:25:29 +0530

    Initial backend setup: Express server, MongoDB connection, User model with password hashing

---
+++ server/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req,res) => {
+    res.send('Job Portal API is running...');
+});
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
