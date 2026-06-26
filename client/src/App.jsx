3a42e1dab75b9cc7015ff782cc6ba312d3cab586
Gohel Nayan <gohelnayan264-gmail@gmail.com>
2026-06-26 16:03:24 +0530

    feat: add JobsList page fetching jobs from backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
+import JobsList from './pages/JobsList';
 
 function App() {
   return (
     <Routes>
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />}></Route>
+      <Route path='/jobs' element={<JobsList />}></Route>
     </Routes>
   );
 }
---
+++ client/src/pages/JobsList.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react';
+import axiosInstance from '../utils/axiosInstance';
+import { Link } from 'react-router-dom';
+
+const JobsList = () => {
+    const [jobs, setJobs] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        const fetchJobs = async () => {
+            try {
+                const response = await axiosInstance.get('/jobs/get-all-jobs');
+                setJobs(response.data.jobs);
+            } catch (err) {
+                setError('Failed to load jobs');
+                console.error(err);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchJobs();
+    }, []);
+
+    if(loading){
+        return <p className="text-center mt-10">Loading jobs...</p>;
+    }
+
+    if(error){
+        return <p className="text-center mt-10 text-red-600">{error}</p>;
+    }
+
+    return (
+    <div className="max-w-4xl mx-auto mt-10 px-4">
+      <h2 className="text-2xl font-bold mb-6">Available Jobs</h2>
+
+      {jobs.length === 0 ? (
+        <p>No jobs posted yet.</p>
+      ) : (
+        <div className="space-y-4">
+          {jobs.map((job) => (
+            <Link
+              to={`/jobs/${job._id}`}
+              key={job._id}
+              className="block bg-white p-4 rounded-lg shadow hover:shadow-md transition"
+            >
+              <h3 className="text-lg font-semibold">{job.title}</h3>
+              <p className="text-gray-600">{job.company} - {job.location}</p>
+              <p className="text-gray-800 mt-2">₹{job.salary.toLocaleString()} / year</p>
+              <span className="inline-block mt-2 text-xs bg-blue-100 text-blue-700 px-2 py-1 rounded">
+                {job.jobType}
+              </span>
+            </Link>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default JobsList;
